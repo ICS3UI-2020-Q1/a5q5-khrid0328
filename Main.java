416cde778eb3fde8efff78cd0fdc112a1fe82c48
khrid0328 <khrid0328@wrdsb.ca>
2020-10-07 02:14:05 +0000

    A5Q5 This program will ask the user for a number between 1 and 10. It will then draw a line of * that is the size that was asked for.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will ask the user for a number between 1 and 10. It will then draw a line of * that is the size that was asked for.
+ * @author Dafna Khripun
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to enter a number between 1 and 10
+    System.out.println("Please enter a number between 1 and 10");
+
+    // create a variable for user input
+    int number = input.nextInt();
+
+    // use a for loop to draw a line of *
+    for(int i = 1; i <= number; i++){
+
+      // loop action: print *
+      System.out.print("* ");
+    }
   }
 }
